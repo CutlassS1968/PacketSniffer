b0f28729d97908ba01eb3c12e567523fa582a409
Evan Johns <43017805+CutlassS1968@users.noreply.github.com>
2022-10-14 19:13:17 -0400

    Update main.c
    maybe works idk ive been trying for 7 hours and i think it works but sometimes it receives weird information.

--- main.c
+++ main.c
@@ -9,32 +9,19 @@
 #include <net/if.h>
 #include <stdlib.h>
 #include <netpacket/packet.h>
+#include <netinet/ip.h>
+#include <netinet/ether.h>
 
-// https://man7.org/linux/man-pages/man7/packet.7.html
-
-void printNetworkInterfaces();
-
+// Useful information: "ip addr show" will show you addresses for debugging
 int main(int argc, char* argv[]) {
 
-    /** Helper code for finding network interfaces **/
-//    if (argc != 2) {
-//        printf("Usage: %s [interface name]\n", argv[0]);
-//        return 1;
-//    }
-//
-//    uint if_index;
-//    if_index = if_nametoindex(argv[1]);
-//    if (if_index == 0) {
-//        printf("Interface %s : No such device \b", argv[1]);
-//        return 1;
-//    }
+    if (argc != 2) {
+        printf("[ERROR]: Usage: ./sniffer <network_interface>\n");
+        return 0;
+    }
 
-//    printf("Interface %s : %d\n", argv[1], if_index);
-
-//    printNetworkInterfaces();
-
-    /** END of helper code **/
-
+    char* if_name = (char*)malloc(100*sizeof(char));
+    strcpy(if_name, argv[1]);
 
     int s = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
     if (s < 0) {
@@ -43,8 +30,10 @@ int main(int argc, char* argv[]) {
     }
 
     // use helper code to find interface names
-    const char* if_name = "lo";
+    // Two interfaces on CIS457 RDP: ens160 and lo
     int if_index = (int)if_nametoindex(if_name);
+
+    free(if_name);
 
     struct sockaddr_ll llAddr;
     llAddr.sll_family = AF_PACKET;
@@ -57,34 +46,53 @@ int main(int argc, char* argv[]) {
         printf("[ERROR]: Unable to bind to socket\n");
     }
 
-    char buffer[1024];
+    printf("[UPDATE]: Listening for incoming traffic...\n");
+
+    unsigned char *buffer = (unsigned char *)malloc(65536);
+    struct ether_header *eh = (struct ether_header *) buffer;
+    struct iphdr *iph = (struct iphdr *) (buffer + sizeof(struct ether_header));
+
     while (1) {
         socklen_t len = sizeof(llAddr);
         struct sockaddr_ll inAddr;
-        ssize_t iBytesSize = recvfrom(s, &inAddr, sizeof(inAddr), 0, (struct sockaddr*)&llAddr, &len);
 
+        ssize_t iBytesSize = recvfrom(s, buffer, 65536, 0, (struct sockaddr*)&llAddr, &len);
+        
+        // get the ethernet header location in the buffer
+        if (iBytesSize == -1) {
+            perror("recvfrom");
+            return 1;
+        } else if (iBytesSize == sizeof(buffer)) {
+            printf("[ERROR]: Frame is too large for buffer");
+        } else {
+            // Handle frame
+            if (inAddr.sll_pkttype != PACKET_OUTGOING) {
+            	
+                memcpy(eh, (struct ether_header *) buffer, sizeof(struct ether_header));
+                printf("[INCOMING]:\n");
+                printf("Frame Size = %ld bytes\n", iBytesSize);
+                printf("Destination MAC Address = %s\n", ether_ntoa((struct ether_addr*)eh->ether_dhost));
+                printf("Source MAC Address = %s\n", ether_ntoa((struct ether_addr*)eh->ether_shost));
+                printf("Type = 0x%04x\n", eh->ether_type);
+
+                // If Ethernet packet is type IPv4 (0x0800 or ETH_P_IP), print src and dest addresses
+                if (eh->ether_type == ETH_P_IP) {
+                    // Get the ip header from its location in the buffer
+                    memcpy(iph, (struct iphdr *) (buffer + sizeof(struct ether_header)), sizeof(struct iphdr));
+                    uint32_t ip;
+                    ip = iph->saddr;
+                    printf("IPv4: Source Address = %s\n", inet_ntoa(*(struct in_addr*)&ip));
+                    ip = iph->daddr;
+                    printf("IPv4: Destination Address = %s\n", inet_ntoa(*(struct in_addr*)&ip));
+                    memset(iph, 0, sizeof(struct iphdr));
+                }
+                memset(eh, 0, sizeof(struct ether_header));
+                printf("\n\n");
+            }
+        }
+        memset(buffer, 0, 65536);
     }
+    close(s);
 
     return 0;
-}
-
-// https://programmer.ink/think/ifname-of-network-interface-and-ifndex-query-of-interface-index-under-linux.html
-void printNetworkInterfaces() {
-    struct if_nameindex *head, *ifni;
-    ifni = if_nameindex();
-    head = ifni;
-
-    if (head == NULL) {
-        perror("if_nameindex()");
-        exit(EXIT_FAILURE);
-    }
-
-    while (ifni->if_index != 0) {
-        printf("Interfece %d : %s\n", ifni->if_index, ifni->if_name);
-        ifni++;
-    }
-
-    if_freenameindex(head);
-    head = NULL;
-    ifni = NULL;
 }
